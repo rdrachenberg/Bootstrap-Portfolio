32f0461a40770c7522b40173a6b51d356f0711bf
Ryan Drachenberg <ryan@Ryans-MacBook-Pro.local>
2018-05-25 13:15:58 -0400

    changed some images

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,10 @@
 // // animate the Ryan Drachenberg Image ***************************
 $(document).ready(function(){
     $("#imgDiv").hover(function(){
-        $("#bioPic").stop().animate({ height: "+=25%", width: "+=25%" });
+        $("#bioPic").stop().animate({ height: "68%", width: "68%" });
     },
     function(){
-        $("#bioPic").stop().animate({ height: "-=25%", width: "-=25%" });
+        $("#bioPic").stop().animate({ height: "133.25px", width: "200px" });
     });
 });
 // end animation the Ryan Drachenberg Image text***************************
