ab8f342fcd00e60910fbc64011b5e2ea78e341af
Ryan Drachenberg <ryan@Ryans-MacBook-Pro.local>
2018-06-04 11:06:44 -0400

    fixing deployed api post routes

--- server.js
+++ server.js
@@ -54,12 +54,12 @@ app.use(bodyParser.json());
 
 
 // Set up CORS
-// app.use(cors()) //
-// app.use("/", api_routes);
-// app.use(function (req, res, next) { res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
+app.use(cors()) //
+app.use("/", api_routes);
+app.use(function (req, res, next) { res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 // seed db
 // db.User.create({
 //     name: "Tester McFly",
