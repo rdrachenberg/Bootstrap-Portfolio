5cfc84b649bda0130405100159630b78713ada45
Ryan Drachenberg <ryan@Ryans-MacBook-Pro.local>
2018-06-04 22:58:32 -0400

    added thanks page and fixed route after contact submission

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -21,7 +21,7 @@ module.exports = {
       .then(dbModel => {
         // res.json(dbModel)
         console.log(dbModel)
-        res.redirect("/")
+        res.redirect("/thanks.html")
       })
       .catch(err => res.status(422).json(err));
   },
